c846705e80dbaaeec6a484c237c55419adffdb33
deroy young <deroy2459@gmail.com>
2021-12-14 20:28:06 -0500

    Components part 2

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -3,13 +3,13 @@ import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 
 function Directory(props) {
-
-    const renderDirectoryItem = ({item}) => {
+    const renderDirectoryItem = ({ item }) => {
         return (
             <ListItem
                 title={item.name}
                 subtitle={item.description}
-                leftAvatar={{ source: require('./images/react-lake.jpg')}}
+                onPress={() => props.onPress(item.id)}
+                leftAvatar={{ source: require('./images/react-lake.jpg') }}
             />
         );
     };
@@ -18,7 +18,7 @@ function Directory(props) {
         <FlatList
             data={props.campsites}
             renderItem={renderDirectoryItem}
-            keyExtractor={item => item.id.toString()}
+            keyExtractor={(item) => item.id.toString()}
         />
     );
 }
