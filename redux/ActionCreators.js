2dfab757b03b87890acdeffcb0c85e9c2e754d35
deroy young <deroy2459@gmail.com>
2021-12-31 19:19:20 -0500

    Alert.

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -175,3 +175,7 @@ export const addFavorite = campsiteId => ({
     payload: campsiteId
 })
 
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
+});
